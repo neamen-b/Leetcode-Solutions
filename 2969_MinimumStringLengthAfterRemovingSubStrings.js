405da7fc2aad2d606d9ea4bff9169382b70b0df7
neamen-b <neamenr@gmail.com>
2024-09-11 02:05:09 -0500

    Solution

---
+++ 2969_MinimumStringLengthAfterRemovingSubStrings.js
@@ -0,0 +1,60 @@
+/**
+ * Initial thoughs:
+ * 1. Use a stack
+ *      Iteratte over s and under certain conditions, either add or pop from the stack
+ * 
+ * 2. If s is empty, return 0
+ * 
+ * 3. For each character in s
+ *      If B look at stack[end]
+ *          If stack[end] is A, stack.pop()
+ *      If D, look at stack[end]
+ *          If stack[end] is C, stack.pop()
+ *      else
+ *          stack.push(character)
+ * 
+ * 
+ * 3. 
+ * 
+ */
+
+
+/**
+ * @param {string} s
+ * @return {interger}
+ */
+
+
+let minLength = function(s){
+
+    // stack to store valid characters
+    character_stack = [];
+
+    // Neat wat of accessing the last element in an array
+    // console.log(character_stack.at(-1))
+
+    // If string is empty
+    if (s.length === 0){
+        return 0;
+    };
+
+    // For each character in s
+    for (let character of s){
+
+        // If char is 'D' and character before it is 'C', remove 'C' and don't add 'D'
+        if (character === 'D' && character_stack.at(-1) === 'C'){
+            character_stack.pop();
+        }
+        // If char is 'B' and character before is 'A', remove 'A' and don't add 'B'
+        else if (character === 'B' && character_stack.at(-1) === 'A'){
+            character_stack.pop();
+        }
+        else {
+            character_stack.push(character);
+        }
+    }
+
+    return character_stack.length;
+}
+
+console.log(minLength(''));
